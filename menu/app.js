1898eb4a5646498c7a6906757a3a61d6f8fd32b0
Jerry Pageot <jerrypageot@gmail.com>
2021-01-28 22:41:26 -0500

    added full js functionality to menu project

--- menu/app.js
+++ menu/app.js
@@ -76,9 +76,32 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+//load items
 window.addEventListener('DOMContentLoaded',function(){
   displayMenuItems(menu)
-})
+});
+
+//filter buttons
+filterBtns.forEach(function(btn){
+  btn.addEventListener('click',function(e){
+    const category = e.currentTarget.dataset.id;
+
+    const menuCategory = menu.filter(function(menuItem){
+      if(menuItem.category === category){
+        return menuItem
+      }
+    });
+    if(category === 'all'){
+      displayMenuItems(menu)
+    }
+    else{
+      displayMenuItems(menuCategory)
+    }
+  });
+});
+
 
 
 
