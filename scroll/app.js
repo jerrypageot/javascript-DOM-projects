c29c4ce5e91a0cec9aa72b7c0c9f5276e12ebd9b
Jerry Pageot <jerrypageot@gmail.com>
2021-02-01 01:26:09 -0500

    added more js functionality to scroll project

--- scroll/app.js
+++ scroll/app.js
@@ -22,8 +22,56 @@ navToggle.addEventListener('click',function(){
     }else{
         linksContainer.style.height = 0;
     }
-})
+});
+
 // ********** fixed navbar ************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll',function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav')
+    }else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeight > 450){
+        topLink.classList.add('show-link')
+    }else{
+        topLink.classList.remove('show-link')
+    }
+})
 
 // ********** smooth scroll ************
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link){
+    link.addEventListener('click',function(e){
+        //prevent default
+        e.preventDefault();
+        //navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        //calculate the height
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav')
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav){
+            position = position - navHeight
+        }
+        if(navHeight > 82){
+            position = position + containerHeight
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    })
+})
 // select links
