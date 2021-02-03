0a7fa4546f03d883b20cf70fcca324b36249c975
Jerry Pageot <jerrypageot@gmail.com>
2021-02-03 12:55:07 -0500

    added new js function to coutdown timer

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -28,10 +28,14 @@ const giveaway = document.querySelector('.giveaway');
 const dealine = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
-let futureDate = new Date(2021,5,12,11,30,0);
+// let futureDate = new Date(2021,5,12,11,30,0);
 
-
+const futureDate = new Date(tempYear,tempMonth,tempDay + 10, 11, 30, 0)
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -43,7 +47,7 @@ const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
 
 
 // future time in miliseconds
