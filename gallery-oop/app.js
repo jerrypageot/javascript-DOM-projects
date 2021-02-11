34121958f4003ffb2f4b75cdce57d4f7ff1b5921
Jerry Pageot <jerrypageot@gmail.com>
2021-02-10 21:46:05 -0500

    added additional js to gallery project

--- gallery-oop/app.js
+++ gallery-oop/app.js
@@ -4,13 +4,49 @@ function getElement(selection) {
     return element;
   }
   throw new Error(
-    `Please check "${selection}" selector, no such element exists`
+    `Please check "${selection}" selector, no such element exist`
   );
 }
 
 
 function Gallery(element){
-  console.log(element);
+  this.container = element;
+  this.list = [...element.querySelectorAll('.img')];
+  //target
+  this.modal = getElement('.modal');
+  this.modalImg = getElement('.main-img');
+  this.iamgeName = getElement('.image-name')
+  this.modalImages = getElement('.modal-images');
+  this.closeBtn = getElement('.close-btn');
+  this.prevBtn = getElement('.prev-btn');
+  this.nextBtn = getElement('.next-btn');
+
+  //bind functions
+  // this.openModal = this.openModal.bind(this);
+
+  this.container.addEventListener('click', function(e){
+    if(e.target.classList.contains('img')){
+      this.openModal(e.target, this.list)
+    }
+  }.bind(this));
+}
+
+Gallery.prototype.openModal = function(selectedImage,list){
+  this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list.map(function(image){
+    return `<img
+     src="${image.src}" 
+     title="${image.title}"
+     data-id="${image.dataset.id}"
+     class="${selectedImage.dataset.id === image.dataset.id?"modal-img selected":"modal-img"}"
+     />`
+  }).join('')
+  this.modal.classList.add('open');
+}
+
+Gallery.prototype.setMainImage = function(selectedImage){
+  this.modalImg.src = selectedImage.src;
+  this.iamgeName.textContent = selectedImage.title;
 }
 
 
